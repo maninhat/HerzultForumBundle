1851196a2170c166e0847accbcfc2e5d54702568
m.kulebyakin <kulebyakin@gmail.com>
2012-08-27 14:58:57 +0400

    getting topic list

--- Entity/TopicRepository.php
+++ Entity/TopicRepository.php
@@ -50,7 +50,8 @@ class TopicRepository extends ObjectRepository implements TopicRepositoryInterfa
         $qb = $this->createQueryBuilder('topic');
         $qb->orderBy('topic.isPinned', 'DESC')
             ->addOrderBy('topic.pulledAt', 'DESC')
-            ->where($qb->expr()->eq('topic.category', $category->getId()));
+             ->innerJoin('topic.category','cat')
+            ->where($qb->expr()->eq('topic.cat', $category->getId()));
          /*
         if ($asPaginator) {
             return new Pagerfanta(new DoctrineORMAdapter($qb->getQuery()));
